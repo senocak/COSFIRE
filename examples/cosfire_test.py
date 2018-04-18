9a4ef5005005b59afe9d91bd697b1d55f001190c
Theys96 <accounts@thijshavinga.nl>
2018-04-18 18:30:55 +0200

    Actually calculate tuples now, along with coordinates

--- examples/cosfire_test.py
+++ examples/cosfire_test.py
@@ -6,7 +6,7 @@ import numpy as np
 import math as m
 import time
 
-proto = np.asarray(Image.open('prototype4.png').convert('L'), dtype=np.float64)
+proto = np.asarray(Image.open('prototype1.png').convert('L'), dtype=np.float64)
 
 sigma = 2.6
 filt = cosfire.DoGFilter(sigma,1)
@@ -18,15 +18,17 @@ for (x,y), value in np.ndenumerate(protoDoG):
 	protoDoG[x,y] = 0 if value < 0.25*maxVal else value;
 
 # Find tuples
-rhoList = [0,10,20,40]
+rhoList = [0,20,40]
 (cx, cy) = (50,50)
 tuples = []
+tupleCoords = []
 precision = 16
 peakFunction = cosfire.CircularPeaksFunction()
 for rho in rhoList:
 	if rho == 0:
 		if protoDoG[cy,cx] > 0:
-			tuples.append([cx, cy])
+			tuples.append((sigma, rho, 0))
+			tupleCoords.append((cy, cx));
 	elif rho > 0:
 		# Compute points (amount=precision) on the circle of radius rho with center point (cx,cy)
 		coords = [ ( cy+int(round(rho*m.sin(phi))) , cx+int(round(rho*m.cos(phi))) )
@@ -37,11 +39,15 @@ for rho in rhoList:
 
 		# Find peaks in circle
 		maxima = peakFunction.transform(vals)
-		tuples.extend([coords[i] for i in maxima])
-		
+		tuples.extend([ (sigma, rho, i*m.pi/precision*2) for i in maxima])
+		tupleCoords.extend([ coords[i] for i in maxima])
+
+print("Tuples:")
+for tupl in tuples:
+	print(tupl)
 
 # Draw image
 plt.imshow(protoDoG, cmap='gray')
-tuples = np.asarray(tuples)
-plt.plot(tuples[:,1], tuples[:,0],"xr")
+tupleCoords = np.asarray(tupleCoords)
+plt.plot(tupleCoords[:,1], tupleCoords[:,0],"xr")
 plt.show()
